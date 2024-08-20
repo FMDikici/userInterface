00bb65418e519d5266e0110aeb505009e40efc0e
Fatih Mehmet Dikici <112941228+FMDikici@users.noreply.github.com>
2024-08-20 15:25:48 +0300

    added

---
+++ app/src/main/java/com/fmd/kullaniciarayuzu/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.fmd.kullaniciarayuzu
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+lateinit var usernameInput:EditText
+lateinit var passwordInput:EditText
+lateinit var loginBtn:Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        usernameInput=findViewById(R.id.username_input)
+        passwordInput=findViewById(R.id.password_input)
+        loginBtn=findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener{
+            val username= usernameInput.text.toString()
+            val password= passwordInput.text.toString()
+            Log.i("Test Credentials","Username: $username and Password $password")
+            //logcat gösterimi
+
+        }
+    }
+}
